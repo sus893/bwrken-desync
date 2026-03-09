5d3fb3bf64d83ab802591a20ad03f35ccb60b129
sus893 <132160188+sus893@users.noreply.github.com>
2026-03-09 15:26:19 +0100

    Update bwrken_desync.lua

--- bwrken_desync.lua
+++ bwrken_desync.lua
@@ -8,6 +8,7 @@ local PlayerGui = Player:WaitForChild("PlayerGui")
 -- Blacklisted users (script silently exits for these players)
 local BLACKLIST = {
     ["Omar20145098"] = true,
+    ["brainrotabusement1"] = true,
 }
 
 if BLACKLIST[Player.Name] then
